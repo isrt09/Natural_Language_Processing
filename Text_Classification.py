f4d8e2472b040fa556b32d3b5a17e9e3614c563e
mazed <mazedur@gmail.com>
2021-09-07 00:05:14 +0600

    NLP : Text Classification ~ Pickling & Unpickling dataset

--- Text_Classification.py
+++ Text_Classification.py
@@ -13,3 +13,16 @@ nltk.download('stopwords')
 # Importing the dataset
 reviews = load_files('txt_sentoken/')
 X,y = reviews.data,reviews.target
+
+# Pickling the dataset
+with open('X.pickle','wb') as f:
+    pickle.dump(X,f)
+    
+with open('y.pickle','wb') as f:
+    pickle.dump(y,f)
+
+# Unpickling dataset
+X_in = open('X.pickle','rb')
+y_in = open('y.pickle','rb')
+X = pickle.load(X_in)
+y = pickle.load(y_in)
