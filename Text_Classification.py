3ea6ab3429632dbb606f0eaea8da065fda700f30
mazed <mazedur@gmail.com>
2021-09-09 23:44:27 +0600

    NLP : Text Classification - BOW Model

--- Text_Classification.py
+++ Text_Classification.py
@@ -39,3 +39,19 @@ for i in range(0, 2000):
     review = re.sub(r'^b\s+', '', review)
     review = re.sub(r'\s+', ' ', review)
     corpus.append(review)    
+
+# Creating the BOW model
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer(max_features = 2000, min_df = 3, max_df = 0.6, stop_words = stopwords.words('english'))
+X = vectorizer.fit_transform(corpus).toarray()
+
+# Creating the Tf-Idf Model
+from sklearn.feature_extraction.text import TfidfTransformer
+transformer = TfidfTransformer()
+X = transformer.fit_transform(X).toarray()
+
+
+# Creating the Tf-Idf model directly
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(max_features = 2000, min_df = 3, max_df = 0.6, stop_words = stopwords.words('english'))
+X = vectorizer.fit_transform(corpus).toarray()
